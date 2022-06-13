0035b7a7445f7c61749c872357cb9972a2d5a566
Eliabe-ciandt <eliabe@ciandt.com>
2022-06-13 14:44:21 -0300

    Correcao

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -2,7 +2,7 @@ public class PrintStringConcat {
     public static void main(String[] args) {
     	String texto1 = "Holberton";
     	String texto2 = "School";
-    	Strng texto = texto1 + " " + texto2
+    	Strng texto = texto1 + " " + texto2;
         System.out.print("Bem vindo à " + texto + "!\n");
     }
 }
