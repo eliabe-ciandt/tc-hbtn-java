7005b348595849f66d4d71d9ea3355a543e5e3fa
Eliabe-ciandt <eliabe@ciandt.com>
2022-06-14 12:36:58 -0300

    criar status do personagem

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,44 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if(getSaudeAtual() > 0) {
+            this.status = "vivo";
+        }else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        setSaudeAtual(getSaudeAtual() - quantidadeDeDano);
+        if (getSaudeAtual() < 0){
+            setSaudeAtual(0);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        setSaudeAtual(getSaudeAtual() + quantidadeDeCura);
+        if (getSaudeAtual() > 100) {
+            setSaudeAtual(100);
+        }
+    }
+}
