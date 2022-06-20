aa0eb3541aec3d8ef2f8ab1bda5827859aa967ab
Eliabe-ciandt <eliabe@ciandt.com>
2022-06-20 12:30:03 -0300

    Exercicio de impressao do 0 ao 99

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,7 @@
+public class Numeros0a99 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; i++){
+            System.out.print((i<99) ? (i + ", ") : (i + "\n"));
+        }
+    }
+}
