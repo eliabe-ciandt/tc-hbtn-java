b41659ae4a1f52ad0a62193f3f437e9b22bb91aa
Eliabe-ciandt <eliabe@ciandt.com>
2022-06-21 09:56:49 -0300

    Exercicio de exception

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,13 @@
+package divisao;
+
+public class Numero {
+    public static void dividir(int a, int b) {
+        try {
+            int c = (int)a / b;
+        } catch (ArithmeticException e){
+            System.out.println("Nao eh possivel dividir por zero");;
+        } finally {
+            System.out.printf("%d / %d = %d\n", a, b, ((b !=0) ? (int) a/b : 0));
+        }
+    }
+}
