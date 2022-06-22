3627f60f02d89088ec6fe2b85c49a323c7da44b1
Eliabe-ciandt <eliabe@ciandt.com>
2022-06-22 08:52:34 -0300

    cria setor financeiro

---
+++ 0x03/salario/Gerente.java
@@ -0,0 +1,12 @@
+public class Gerente extends Empregado{
+
+    public Gerente(double salarioFixo) {
+        super(salarioFixo);
+    }
+
+    @Override
+    public double calcularBonus(Departamento departamento) {
+        return (departamento.alcancouMeta() ? getSalarioFixo() * 0.2 +
+                (departamento.getValorAtingidoMeta() - departamento.getValorMeta()) * 0.01 : 0);
+    }
+}
