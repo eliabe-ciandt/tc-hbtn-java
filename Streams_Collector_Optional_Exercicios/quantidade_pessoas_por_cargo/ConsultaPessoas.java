901103f9a3688c2a5415fcf052c8f365bc72916a
Eliabe-ciandt <eliabe@ciandt.com>
2022-07-12 16:14:45 -0300

    That was easy

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoaList) {
+        TreeMap<String, TreeSet<Pessoa>> mapaPessoas = pessoaList.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+                () -> new TreeMap<>(Comparator.reverseOrder()), Collectors.toCollection(TreeSet::new)));
+
+        return mapaPessoas;
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        Map<String, Long> mapaPessoas = pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+
+        return mapaPessoas;
+    }
+}
