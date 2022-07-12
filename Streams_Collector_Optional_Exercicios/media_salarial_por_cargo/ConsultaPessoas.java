c4f0e79e930c59faa838057d90672158666381d1
Eliabe-ciandt <eliabe@ciandt.com>
2022-07-12 16:33:54 -0300

    just one more

---
+++ Streams_Collector_Optional_Exercicios/media_salarial_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoaList) {
+        TreeMap<String, TreeSet<Pessoa>> mapaPessoas = pessoaList.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+                () -> new TreeMap<>(Comparator.reverseOrder()), Collectors.toCollection(TreeSet::new)));
+
+        return mapaPessoas;
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        Map<String, Long> mapaPessoas = pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+
+        return mapaPessoas;
+    }
+
+    public static Map<String, Map<Integer,Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas) {
+        Map<String, Map<Integer,Long>> mapaPessoas = pessoas.stream().collect(Collectors
+                .groupingBy(Pessoa::getCargo, Collectors.groupingBy(Pessoa::getIdade, Collectors.counting())));
+
+        return mapaPessoas;
+    }
+
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> pessoas) {
+        Map<String, Double> mapaPessoas = pessoas.stream().collect(Collectors
+                .groupingBy(Pessoa::getCargo, Collectors.averagingDouble(Pessoa::getSalario)));
+
+        return mapaPessoas;
+    }
+}
